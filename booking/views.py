bac5b1d47ab667f02c35c79efe0952b70a03fdae
vikdts <>
2024-02-10 12:34:14 +0000

    add generic view to home page template index.html

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class HomeView(generic.TemplateView):
+    """
+    Add generic view for home page.
+    """
+    template_name = 'index.html'

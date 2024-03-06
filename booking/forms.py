9c9738fcf788fd66c44a04604b72ec9f2f36964a
Viktoriya Tsareva <Vik@Viks-Mac.local>
2024-03-06 13:56:47 +0200

    create calendar input for date

--- booking/forms.py
+++ booking/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from .models import Booking
 from django.forms.widgets import DateInput
 
+class CalendarInput(DateInput):
+    input_type = 'date'
+
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking

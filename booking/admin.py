d68e366c8e065aaa5d4d6a644181b24f94b29849
Viktoriya Tsareva <Vik@Viks-Mac.local>
2024-03-06 13:31:07 +0200

    register booking model in admin

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 from booking.models import Booking, Table
 
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    model = Booking
+    list_display = ('name', 'email_address', 'phone', 'date', 'num_guests', 'time', 'note')

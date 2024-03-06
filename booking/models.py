f57c9a9d5ced7d30fe822d45a121167ec320d610
Viktoriya Tsareva <Vik@Viks-Mac.local>
2024-03-06 12:59:58 +0200

    create choices for time field

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+TIME_CHOICE = (
+    ('18:00', '18:00'),
+    ('18:30', '18:30'),
+    ('19:00', '19:00'),
+    ('19:30', '19:30'),
+    ('20:00', '20:00'),
+    ('20:30', '20:30'),
+    ('21:00', '21:00'),
+)

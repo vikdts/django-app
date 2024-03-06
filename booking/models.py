130c95e5440957e20b54b281f832ba49dc35d335
Viktoriya Tsareva <Vik@Viks-Mac.local>
2024-03-06 13:09:50 +0200

    create a table class with choice for capacity and availability

--- booking/models.py
+++ booking/models.py
@@ -10,3 +10,15 @@ TIME_CHOICE = (
     ('20:30', '20:30'),
     ('21:00', '21:00'),
 )
+
+class Table(models.Model):
+    SEATING_CHOICES = [
+        (1,'1-2 Guests'),
+        (2,'3-4 Guests'),
+        (3,'5-6 Guests'),
+        (4,'7-8 Guests'),
+        (5,'9-10 Guests'),
+        (6,'11-12 Guests'),
+    ]
+    seating_capcity = models.IntegerField(choices=SEATING_CHOICES)
+    available = models.BooleanField(default=True)

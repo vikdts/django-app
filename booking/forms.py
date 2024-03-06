c0fcc1b4ff3cfe104dd444a5d09510a185cc9fc6
Viktoriya Tsareva <Vik@Viks-Mac.local>
2024-03-06 13:53:41 +0200

    create a booking form

--- booking/forms.py
+++ booking/forms.py
@@ -1,3 +1,7 @@
 from django import forms
 from .models import Booking
 
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ['name', 'email_address', 'phone', 'date', 'num_guests', 'time', 'note']

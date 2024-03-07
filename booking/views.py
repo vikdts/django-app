cf12a6fe56702ed6413cedbcc0e3c6f38a20a2bb
Viktoriya Tsareva <Vik@Viks-Mac.local>
2024-03-07 09:02:18 +0200

    add django redirect shortcut

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from django.contrib.auth.decorators import login_required
 from .forms import BookingForm

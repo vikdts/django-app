029da484cfd4bbe6a3d52afb5bba1dc3331dbe76
Viktoriya Tsareva <Vik@Viks-Mac.local>
2024-03-07 11:23:32 +0200

    add delete url path

--- booking/urls.py
+++ booking/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('create_booking/', views.create_booking, name='create_booking'),
     path('bookings', views.read_booking, name='bookings'),
     path('update_booking/<int:pk>/', views.update_booking, name='update_booking'),
+    path('delete_booking/<int:pk>/', views.delete_booking, name='delete_booking'),
 ]

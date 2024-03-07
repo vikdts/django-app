9c83f1414b53f99798ca79a610237f815be9da90
Viktoriya Tsareva <Vik@Viks-Mac.local>
2024-03-07 09:49:49 +0200

    add url path to bookings

--- booking/urls.py
+++ booking/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('create_booking/', views.create_booking, name='create_booking'),
+    path('bookings', views.BookingsView.as_view(), name='bookings'),
 ]

84d3812bbb0feb1bf83e7ba7789a9e2fc2a5e4b7
Viktoriya Tsareva <Vik@Viks-Mac.local>
2024-03-07 11:36:31 +0200

    add url path to menu

--- booking/urls.py
+++ booking/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('bookings', views.read_booking, name='bookings'),
     path('update_booking/<int:pk>/', views.update_booking, name='update_booking'),
     path('delete_booking/<int:pk>/', views.delete_booking, name='delete_booking'),
+    path('menu', views.MenuView.as_view(), name='menu'),
 ]
